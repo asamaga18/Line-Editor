45f502589f1f6377de8f56ff6be1f9894f4e9c16
Amogh Samaga <57379492+asamaga18@users.noreply.github.com>
2025-01-22 17:13:46 -0500

    fixed DLL methods and class

--- myDoublyLinkedList.py
+++ myDoublyLinkedList.py
@@ -1,25 +1,54 @@
-class myDoublyLinkedList:
+class Node:
+    def __init__(self, data = None):
+        self.prev = None
+        self.data = data
+        self.next = None
 
-    class Node:
-        prev = None
-        next = None
-        data = None
+class DoublyLinkedList:
 
-        def __init__(self, data, prev, next):
-            self.prev = prev
-            self.data = data
-            self.next = next
-
-        def __str__(self):
-            if (data == None):
-                print("Data is None")
-
-            else:
-                print(data)
+    def __init__(self):
+        self.head = None
+        self.length = 0
 
     
-    head = None
+    def insertFirst(self, data):
+        
+        newNode = Node(data)
+        newNode.next = self.head
+        self.length = self.length + 1
 
-    def __init__(self, data):
-        pass
+        if self.head:
+            self.head.prev = newNode
+        
+        self.head = newNode
+
+
+    def insertLast(self, data):
+        
+        newNode = Node(data)
+        self.length = self.length + 1
+
+        if self.head is None:
+            self.head =  newNode
+            return
+
+        currNode = self.head
+        while (currNode.next != None):
+            currNode = currNode.next
+            
+        currNode.next = newNode
+        newNode.prev = currNode
+        
+    def getLength(self):
+        return self.length
+    
+    def getHead(self):
+        return self.head
+    
+
+    def printList(self):
+        currNode = self.head
+        while(currNode != None):
+            print(currNode.data)
+            currNode = currNode.next
 
